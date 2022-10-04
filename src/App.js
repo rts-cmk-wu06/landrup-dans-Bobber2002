ec0a5b77496fd97107994e0cacc392ec7f1e83f8
ChristofferRTS <4906240@RTS-365.dk>
2022-10-04 14:34:50 +0200

    add welcome page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import Screen from "./lib/screen";
+import Screen from "./lib/components/screen";
+import WelcomePage from "./pages/Welcome/welcome";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="bg-slate-800 h-screen w-screen">
+      <link
+        href="http://fonts.googleapis.com/css?family=Roboto"
+        rel="stylesheet"
+        type="text/css"
+      />
+      <link
+        href="http://fonts.googleapis.com/css?family=Racing+Sans+One"
+        rel="stylesheet"
+        type="text/css"
+      />
       <Screen>
-        <p>herllo</p>
+        <Router>
+          <Routes>
+            <Route path="/" element={<WelcomePage/>} />
+          </Routes>
+        </Router>
       </Screen>
     </div>
   );
--- src/lib/components/screen.js
+++ src/lib/components/screen.js
@@ -1,7 +1,7 @@
 ﻿const Screen = ({ children }) => {
   return (
     <div className="flex justify-center items-center h-full w-full">
-      <div className="min-h-[823px] max-h-[823px] min-w-[411px] max-w-[411px] bg-white">
+      <div className="min-h-[823px] h-[823px] max-h-[823px] w-[411px] min-w-[411px] max-w-[411px] bg-white">
         {children}
       </div>
     </div>
