003c509807762510311d6f7adb699cfd1cce5258
ChristofferRTS <4906240@RTS-365.dk>
2022-10-05 20:22:28 +0200

    create components of repeated elements + routing fixes

---
+++ src/lib/components/activityCard.js
@@ -0,0 +1,22 @@
+﻿import { Link } from "react-router-dom";
+
+const Activity = ({ activity }) => {
+  return (
+    <Link
+      to={'/user/class?id='+activity.id}
+      className="w-full !h-[344px] bg-white bg-cover bg-center rounded-[39px] rounded-br-none overflow-hidden flex flex-col-reverse"
+      style={{
+        backgroundImage: `url(${activity.asset.url})`,
+      }}
+    >
+      <div className="p-5 bg-secondary bg-opacity-80 text-lg rounded-tr-[39px]">
+        <p>{activity.name}</p>
+        <p>
+          {activity.minAge}-{activity.maxAge} år
+        </p>
+      </div>
+    </Link>
+  );
+};
+
+export default Activity;
