39e01e9b1562c706a28805b7af4645853e74e973
ChristofferRTS <4906240@RTS-365.dk>
2022-10-06 20:45:42 +0200

    add functionality to sigup button in activityOverview using api

--- src/pages/Overview/overview.js
+++ src/pages/Overview/overview.js
@@ -1,7 +1,11 @@
 ﻿import axios from "axios";
 import { useEffect, useState } from "react";
-const ClassOverview = () => {
+const ClassOverview = ({ token }) => {
   const [activity, setActivity] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [usersIds, setUsersIds] = useState([]);
+
+  let sort = [];
 
   let activityId = window.location.search.replace("?id=", "");
 
@@ -9,10 +13,37 @@ const ClassOverview = () => {
     // console.log(activityId);
     axios
       .get("http://localhost:4000/api/v1/activities/" + activityId)
-      .then((response) => setActivity(response.data));
+      .then((response) => {
+        setActivity(response.data);
+        setUsers(response.data.users);
+      });
   }, []);
 
-  // console.log(activity);
+  useEffect(() => {
+    console.log(users);
+    users.map((user) => {
+      sort.push(user.id);
+    });
+    setUsersIds(sort);
+  }, [users, setUsers]);
+
+
+  function handleSignUp() {
+    console.log("token: " + token);
+    axios
+      .post(
+        `http://localhost:4000/api/v1/users/5/activities/${activityId}`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((response) => {
+        // console.log(response);
+      });
+  }
 
   return (
     <div
@@ -26,13 +57,20 @@ const ClassOverview = () => {
             backgroundImage: `url("${activity.asset && activity.asset.url}")`,
           }}
         >
-          <button className="text-center text-white w-[249px] h-[54px] rounded-md bg-primary mb-6 mr-8 shadow-[3px_3px_4px_4px_rgba(0,0,0,0.25)]">
-            Tilmeld
+          <button
+            onClick={handleSignUp}
+            className="text-center text-white w-[249px] h-[54px] rounded-md bg-primary mb-6 mr-8 shadow-[3px_3px_4px_4px_rgba(0,0,0,0.25)]"
+          >
+            {
+              usersIds.includes(5) ? "Tilmeldt" : "Tilmeld"
+            }
           </button>
         </div>
         <div className="text-themewhite px-6 pt-8 pb-24 max-h-[40%]">
           <h2 className="text-2xl leading-5">{activity.name}</h2>
-          <h3 className="text-lg">{activity.minAge}-{activity.maxAge} år</h3>
+          <h3 className="text-lg">
+            {activity.minAge}-{activity.maxAge} år
+          </h3>
           <p className="text-lg pt-2 leading-5 max-h-[160px] overflow-scroll noscrollbar">
             {activity.description}
           </p>
